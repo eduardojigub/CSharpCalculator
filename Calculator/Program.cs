312527db325efc9cdbbebbd06a5a713b06e7e588
eduardojigub <eduardobrunoit@gmail.com>
2022-07-01 11:52:49 -0300

    feat: adding second value and final sum

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,8 +7,14 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("Type the first value: ");
+            Console.WriteLine("Type the first value:");
             float firstValue = float.Parse(Console.ReadLine());
+            Console.WriteLine("Type the second value:");
+            float secondValue = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float SumResult = firstValue + secondValue;
+            Console.WriteLine("The sum result is: " + SumResult);
         }
     }
 }
