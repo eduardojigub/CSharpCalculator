154b5137956af7b833972e9e461cb8acd087cc4f
eduardojigub <eduardobrunoit@gmail.com>
2022-07-01 12:47:53 -0300

    feat: division method

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,7 +7,8 @@ namespace Calculator
         static void Main(string[] args)
         {
             // Sum();
-            Subtraction();
+            // Subtraction();
+            Division();
         }
         static void Sum()
         {
@@ -31,8 +32,21 @@ namespace Calculator
             float secondValue = float.Parse(Console.ReadLine());
 
             Console.WriteLine("");
-            float SumResult = firstValue - secondValue;
-            Console.WriteLine($"The subtraction result is: {SumResult}");
+            float SubtractionResult = firstValue - secondValue;
+            Console.WriteLine($"The subtraction result is: {SubtractionResult}");
+            Console.ReadKey();
+        }
+        static void Division()
+        {
+            Console.Clear();
+            Console.WriteLine("Type the first value:");
+            float firstValue = float.Parse(Console.ReadLine());
+            Console.WriteLine("Type the second value:");
+            float secondValue = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float DivisionResult = firstValue / secondValue;
+            Console.WriteLine($"The sum result is: {DivisionResult}");
             Console.ReadKey();
         }
     }
